8e7e67022b863dc82d782264e6456f7a53f47672
chazo1994 <chazo1994@gmail.com>
2016-10-14 22:19:41 +0700

    add fragment DrinkLog Thinhnv

--- app/src/main/java/com/example/asus/refreshbody/fragment/DrinkLog.java
+++ app/src/main/java/com/example/asus/refreshbody/fragment/DrinkLog.java
@@ -11,41 +11,19 @@ import android.view.ViewGroup;
 import com.example.asus.refreshbody.R;
 
 /**
+ * created by Thinh
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * to handle interaction events.
  * create an instance of this fragment.
  */
 public class DrinkLog extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    /*private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;*/
 
     public DrinkLog() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     */
-    // TODO: Rename and change types and number of parameters
-    /*public static DrinkLog newInstance(String param1, String param2) {
-        DrinkLog fragment = new DrinkLog();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }*/
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,18 +48,5 @@ public class DrinkLog extends Fragment {
         super.onDetach();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    /*public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
+
 }
